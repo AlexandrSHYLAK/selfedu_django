f119d7c1fb1642ef0a94cf1818678818766ebf4d
AlexandrSHYLAK <alekiskander@gmail.com>
2025-10-17 23:03:03 +0300

    #13. Шаблонный тег url

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,5 +1,3 @@
-from multiprocessing.managers import public_methods
-
 from django.urls import path, re_path, register_converter
 from . import views, converters
 
@@ -10,8 +8,12 @@ urlpatterns = [
 
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    # path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    # path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
     # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive),
-    path("archive/<year4:year>/", views.archive, name='archive'),
+    # path("archive/<year4:year>/", views.archive, name='archive'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
