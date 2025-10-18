ba8d8949c6651a54fddb7a0beba579c366e6cf3e
AlexandrSHYLAK <alekiskander@gmail.com>
2025-10-18 23:58:37 +0300

    #19. Понятие CRUD. Добавление записей в таблицу БД. Модуль django-extensions

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=title)
+    time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_pablished = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True)
