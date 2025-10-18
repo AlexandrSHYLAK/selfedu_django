b1084e66034badbd680ddaf4a4b2e4364d398ba0
AlexandrSHYLAK <alekiskander@gmail.com>
2025-10-18 21:54:12 +0300

    #17. Что такое БД, SQL и ORM. Создание первой модели

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=title)
+    time_update = models.DateTimeField(auto_now=True)
+    is_pablished = models.BooleanField(default=True)
