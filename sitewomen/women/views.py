ede6a1029fd84012ecf47d4a4120fb982495c40c
AlexandrSHYLAK <alekiskander@gmail.com>
2025-10-26 14:26:47 +0300

    #51. Введение в CBV (Class Based Views). Классы View и TemplateView

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.context_processors import request
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+from django.views import View
+from django.views.generic import TemplateView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost, UploadFiles
@@ -11,7 +14,8 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
-]
+        ]
+
 
 # data_db = [
 #     {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
@@ -39,11 +43,27 @@ def index(request):
     }
     return render(request, 'women/index.html', context=data)
 
+
+class WomenHome(TemplateView):
+    template_name = 'women/index.html'
+    extra_context = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': Women.published.all().select_related('cat'),
+        'cat_selected': 0,
+    }
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['title'] = 'Главная страница'
+    #     context['menu'] = menu
+    #     context['posts'] = Women.published.all().select_related('cat')
+    #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
+    #     return context
+
 # def handle_uploaded_file(f):
 #     with open(f"uploads/{f.name}", "wb+") as destination:
 #         for chunk in f.chunks():
 #             destination.write(chunk)
-
 
 
 def about(request):
@@ -70,6 +90,7 @@ def show_post(request, post_slug):
     }
     return render(request, 'women/post.html', data)
 
+
 def addpage(request):
     if request.method == "POST":
         form = AddPostForm(request.POST, request.FILES)
@@ -94,12 +115,36 @@ def addpage(request):
     return render(request, 'women/addpage.html', data)
 
 
+class AddPage(View):
+    def get(self, request):
+        form = AddPostForm()
+        data = {
+            'menu': menu,
+            'title': "Добавление статьи",
+            'form': form
+        }
+        return render(request, 'women/addpage.html', data)
+
+    def post(self, request):
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        data = {
+            'menu': menu,
+            'title': "Добавление статьи",
+            'form': form
+        }
+        return render(request, 'women/addpage.html', data)
+
+
 def contact(request):
     return HttpResponse("Обратная связь")
 
 
 def login(request):
     return HttpResponse("Авторизация")
+
 
 def show_category(request, cat_slug):
     category = get_object_or_404(Category, slug=cat_slug)
@@ -115,6 +160,7 @@ def show_category(request, cat_slug):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
 
 def show_tag_postlist(request, tag_slug):
     tag = get_object_or_404(TagPost, slug=tag_slug)
