60a16a71be9dda195ca0cf20fa049401abc66860
AlexandrSHYLAK <alekiskander@gmail.com>
2025-11-09 23:17:01 +0300

    #64. Регистрация пользователей через функции представления

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -7,5 +7,6 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 
 ]
